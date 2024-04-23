e2eaef58fc51bb92747ac709e44fcf8c68811e3c
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-04-23 21:29:33 +0300

    fix : checkStyle failure fixed

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -1,11 +1,7 @@
 package mate.academy.bookstore;
 
-import mate.academy.bookstore.serivce.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -13,6 +9,4 @@ public class BookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
-
-
 }
