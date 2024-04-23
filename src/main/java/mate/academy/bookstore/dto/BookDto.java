2c184a8d82acfc2076ca6c7b564f946d6206b61a
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-04-24 00:08:34 +0300

    fix : added isbn field

--- src/main/java/mate/academy/bookstore/dto/BookDto.java
+++ src/main/java/mate/academy/bookstore/dto/BookDto.java
@@ -8,6 +8,7 @@ public class BookDto {
     private Long id;
     private String title;
     private String author;
+    private String isbn;
     private BigDecimal price;
     private String description;
     private String coverImage;
