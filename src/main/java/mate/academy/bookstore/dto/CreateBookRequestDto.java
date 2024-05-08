af9e6bab9aa5d1a57d56c5808ba89e76e3846e4d
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-05-08 17:57:42 +0300

    feat : completed task

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,23 @@
 package mate.academy.bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @ISBN
+    @NotBlank
     private String isbn;
+    @Min(value = 0)
+    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
