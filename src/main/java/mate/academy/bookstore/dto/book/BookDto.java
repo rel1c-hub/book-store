fb9d71b081649211fcbe9c2462737672ad0a0eba
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-05-10 01:19:13 +0300

    fix : bookDto deleted and created in right place

--- src/main/java/mate/academy/bookstore/dto/book/BookDto.java
+++ src/main/java/mate/academy/bookstore/dto/book/BookDto.java
@@ -1,23 +1,14 @@
-package mate.academy.bookstore.dto;
+package mate.academy.bookstore.dto.book;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
-import org.hibernate.validator.constraints.ISBN;
 
 @Data
-public class CreateBookRequestDto {
-    @NotBlank
+public class BookDto {
+    private Long id;
     private String title;
-    @NotBlank
     private String author;
-    @ISBN
-    @NotBlank
     private String isbn;
-    @Min(value = 0)
-    @NotNull
     private BigDecimal price;
     private String description;
     private String coverImage;
