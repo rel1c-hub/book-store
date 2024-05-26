6dbc905863832a5d0ef1c0e1f00f183d0f3439d3
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-05-26 14:07:26 +0300

    fix : changes due to review

--- src/main/java/mate/academy/bookstore/repository/ShoppingCartRepository.java
+++ src/main/java/mate/academy/bookstore/repository/ShoppingCartRepository.java
@@ -3,7 +3,9 @@ package mate.academy.bookstore.repository;
 import java.util.Optional;
 import mate.academy.bookstore.model.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
     Optional<ShoppingCart> findByUserId(Long userId);
 }
--- src/main/java/mate/academy/bookstore/serivce/shoppingcart/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/bookstore/serivce/shoppingcart/ShoppingCartServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.bookstore.serivce.shoppingcart;
 
+import jakarta.transaction.Transactional;
 import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.dto.cartitem.CartItemResponseDto;
@@ -30,6 +31,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     @Override
+    @Transactional
     public CartItemResponseDto addToCart(CreateCartItemRequestDto requestDto, Long userId) {
         ShoppingCart shoppingCart = getShoppingCartForUser(userId);
         CartItem cartItem = cartItemService.addToCart(requestDto, shoppingCart);
@@ -38,6 +40,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     @Override
+    @Transactional
     public CartItemResponseDto updateCart(UpdateCartItemRequestDto requestDto,
                                           Long cartItemId, Long userId) {
         ShoppingCart shoppingCart = getShoppingCartForUser(userId);
@@ -47,6 +50,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     }
 
     @Override
+    @Transactional
     public void deleteCartItem(Long cartItemId, Long userId) {
         ShoppingCart shoppingCart = getShoppingCartForUser(userId);
         CartItem cartItem = shoppingCart.getCartItems().stream()
