5921a7b317245f948a9b78f26d44ac7e9175da64
Pavlo Serikov <113522310+rel1c-hub@users.noreply.github.com>
2024-05-28 21:57:41 +0300

    Revert "feat : repositories adedd"
    This reverts commit acd75e188f20c4d54d1745e67d6dcce5066023b0.

--- src/main/java/mate/academy/bookstore/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.bookstore.repository;
-
-import java.util.Optional;
-import mate.academy.bookstore.model.ShoppingCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-    Optional<ShoppingCart> findByUserId(Long userId);
-}
